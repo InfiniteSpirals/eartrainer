f2438b8a2bef24d7159cb6d39f13af0c26e8e5e1
chris hall <chris.hall@nxtds.com>
2022-04-22 09:45:41 +0100

    Add project files.

---
+++ Aud.io/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aud.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
