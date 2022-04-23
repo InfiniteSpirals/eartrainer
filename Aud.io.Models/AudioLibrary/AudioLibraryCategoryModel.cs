cfedc3fad6d7449913e8762da8d410b9c53c8000
chris hall <chris.hall@nxtds.com>
2022-04-23 17:51:59 +0100

    Added tracking for last 3 guesses to spread out more evenly in the freq range

--- Aud.io.Models/AudioLibrary/AudioLibraryCategoryModel.cs
+++ Aud.io.Models/AudioLibrary/AudioLibraryCategoryModel.cs
@@ -8,6 +8,11 @@ namespace Aud.io.Models.AudioLibrary
 {
     public class AudioLibraryCategoryModel
     {
+        public AudioLibraryCategoryModel()
+        {
+            Tracks = new List<AudioLibraryEntryModel>();
+        }
+
         public string                               Label   { get; set; }
         public IEnumerable<AudioLibraryEntryModel>  Tracks  { get; set; }
     }
---
+++ Aud.io/Api/Controllers/LibraryController.cs
@@ -0,0 +1,45 @@
+﻿using Aud.io.Models.AudioLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aud.io.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public LibraryController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        [HttpGet]
+        public JsonResult GetLibrary()
+        {
+            // Map Path
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "audio");
+
+            // create return model
+            var model = new List<AudioLibraryCategoryModel>();
+
+            // list all directories
+            var directories = Directory.GetDirectories(path);
+
+
+            foreach(var d in directories)
+            {
+                // dir name
+                var dirName = Path.GetFileName(d);
+                // get all the tracks
+                var tracks = Directory.GetFiles(d).Select(x => new AudioLibraryEntryModel() { Filename = Path.GetFileName(x), Filepath = $"/audio/{dirName}/{Path.GetFileName(x)}" });
+
+                // create a directory model
+                model.Add(new AudioLibraryCategoryModel() { Label = dirName, Tracks = tracks });
+            }
+            
+            return Json(model);
+        }
+    }
+}
