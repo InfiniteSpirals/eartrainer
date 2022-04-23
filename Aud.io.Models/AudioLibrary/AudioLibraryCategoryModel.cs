a33d23f9577ede8fd6161fa066a6ddf87cd2cbef
chris hall <chris.hall@nxtds.com>
2022-04-23 16:23:48 +0100

    Add Models Class Lib + Git Ignore music!

---
+++ Aud.io.Models/AudioLibrary/AudioLibraryCategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aud.io.Models.AudioLibrary
+{
+    public class AudioLibraryCategoryModel
+    {
+        public string                               Label   { get; set; }
+        public IEnumerable<AudioLibraryEntryModel>  Tracks  { get; set; }
+    }
+}
---
+++ Aud.io.Models/AudioLibrary/AudioLibraryEntryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aud.io.Models.AudioLibrary
+{
+    public class AudioLibraryEntryModel
+    {
+        public string Filename  { get; set; }
+        public string Filepath  { get; set; }
+        public string Label     { get; set; }
+    }
+}
